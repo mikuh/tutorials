4422d4458bab58b40421348ccc5663404c0f7214
geb <853934146@qq.com>
2021-03-16 18:44:41 +0800

    update

--- DRL/agents/dqn/target_dqn.py
+++ DRL/agents/dqn/target_dqn.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
+from types import SimpleNamespace
 import numpy as np
 from collections import deque
 from DRL.agents import Agent
+import random
 import gym
 
 
@@ -19,56 +21,101 @@ class DQN(tf.keras.Model):
         return value
 
 
-
 class TargetDQNAgent(object):
 
-    def __init__(self, env_config):
+    def __init__(self, env_config: SimpleNamespace):
         self.lr = env_config.lr
         self.gamma = env_config.gamma
-        self.epsilon = env_config.epsilon
+
+        self.env = gym.make(env_config.env)
+
+        self.batch_size = env_config.batch_size
+        self.state_size = self.env.observation_space.shape[0]
+        self.action_size = self.env.action_space.n
+
+        self.memory = deque(maxlen=2000)
 
         self.dqn_model = DQN()
         self.dqn_target = DQN()
         self.opt = tf.keras.optimizers.Adam(lr=self.lr, )
 
-        self.batch_size = 64
-        self.state_size = 4
-        self.action_size = 2
-
-        self.memory = deque(maxlen=2000)
-
-        self.env = gym.make('CartPole-v1')
-
         self.score = 0
+        self.steps = 0
+        self.episodes = 0
 
     def collect_transitions(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
     def get_action(self, obs):
+        q_value = self.dqn_model(np.array([obs], dtype=np.float32))[0]
+        if np.random.rand() <= self.epsilon:
+            action = np.random.choice(self.action_size)
+        else:
+            action = np.argmax(q_value)
+        return action
 
-        return 1
+    def update(self):
+        mini_batch = random.sample(self.memory, self.batch_size)
+        states, actions, rewards, next_states, dones = zip(*[(e[0], e[1], e[2], e[3], e[4]) for e in mini_batch])
+
+        dqn_variable = self.dqn_model.trainable_variables
+        with tf.GradientTape() as tape:
+            tape.watch(dqn_variable)
+
+            rewards = np.array(rewards, dtype=np.float32)
+            actions = np.array(actions, dtype=np.int32)
+            dones = np.array(dones, dtype=np.float32)
+
+            target_q = self.dqn_target(tf.convert_to_tensor(np.vstack(next_states), dtype=tf.float32))
+            next_action = tf.argmax(target_q, axis=1)
+            target_value = tf.reduce_sum(tf.one_hot(next_action, self.action_size) * target_q, axis=1)
+
+            target_value = (1 - dones) * self.gamma * target_value + rewards
+
+            main_q = self.dqn_model(tf.convert_to_tensor(np.vstack(states), dtype=tf.float32))
+            main_value = tf.reduce_sum(tf.one_hot(actions, self.action_size) * main_q, axis=1)
+
+            error = tf.square(main_value - target_value) * 0.5
+            error = tf.reduce_mean(error)
+
+        dqn_grads = tape.gradient(error, dqn_variable)
+        self.opt.apply_gradients(zip(dqn_grads, dqn_variable))
+
+        if self.steps % 20 == 0:
+            self.dqn_target.set_weights(self.dqn_model.get_weights())
+
+    @property
+    def epsilon(self):
+        return 1 / (self.episodes * 0.1 + 1)
 
     def learn(self):
 
-        episodes = 0
-        steps = 0
         while True:
             obs = self.env.reset()
             done = False
             score = 0
+            self.episodes += 1
             while not done:
                 action = self.get_action(obs)
+                self.steps += 1
                 next_state, reward, done, _ = self.env.step(action)
                 self.collect_transitions(obs, action, reward, next_state, done)
-                steps += 1
                 score += reward
-
                 obs = next_state
-
                 if len(self.memory) > self.batch_size:
                     self.update()
-                    if steps % 20 == 0:
-                        self.update_target()
+            print(score)
 
 
+if __name__ == '__main__':
+    env_config = {
+        "env": "CartPole-v1",
+        "lr": 0.001,
+        "gamma": 0.99,
+        "batch_size": 64
+    }
 
+    env_config = SimpleNamespace(**env_config)
+
+    agent = TargetDQNAgent(env_config)
+    agent.learn()
